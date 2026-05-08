4b339ac0a527bd557e32211c400fba0d68561204
Christian Henriquez <disweb.chris@gmail.com>
2026-05-07 22:17:16 -0300

    fix: vite config for Firebase initialization order

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/entrenador/",
+  optimizeDeps: {
+    include: ["firebase/app", "firebase/auth", "firebase/firestore"],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/firebase/, /node_modules/],
+    },
+  },
 });
