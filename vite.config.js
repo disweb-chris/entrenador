b40cb7781d8e9911c791e42b716b9ad1e6adc024
Christian Henriquez <disweb.chris@gmail.com>
2026-05-15 00:04:06 -0300

    fix: split firebase into separate chunk

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/entrenador/",
-  optimizeDeps: {
-    include: ["firebase/app", "firebase/auth", "firebase/firestore"],
-  },
   build: {
-    commonjsOptions: {
-      include: [/firebase/, /node_modules/],
-    },
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+        entryFileNames: `assets/[name]-[hash]-v3.js`,
+        chunkFileNames: `assets/[name]-[hash]-v3.js`,
+        manualChunks: {
+          "firebase": ["firebase/app", "firebase/auth", "firebase/firestore"],
+          "react-vendor": ["react", "react-dom"],
+        },
       },
     },
   },
